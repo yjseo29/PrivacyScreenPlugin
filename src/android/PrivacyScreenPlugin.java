a4137e53fc495d2f74e5f1b238088ae8bfd5d1e6
YJ SEO <youngjin2960@gmail.com>
2018-10-03 23:03:22 +0900

    Update PrivacyScreenPlugin.java

--- src/android/PrivacyScreenPlugin.java
+++ src/android/PrivacyScreenPlugin.java
@@ -6,21 +6,13 @@
  */
 package org.devgeeks.privacyscreen;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaWebView;
-
 import android.app.Activity;
-import android.view.Window;
+import android.content.SharedPreferences;
 import android.view.WindowManager;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.content.Context;
-import android.os.Bundle;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 
 /**
  * This class sets the FLAG_SECURE flag on the window to make the app
@@ -28,10 +20,19 @@ import android.os.Bundle;
  */
 public class PrivacyScreenPlugin extends CordovaPlugin {
 
+  private SharedPreferences sharedPref;
+
   @Override
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
-    Activity activity = this.cordova.getActivity();
-    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+
+    sharedPref = cordova.getActivity().getSharedPreferences("NativeStorage", Activity.MODE_PRIVATE);
+
+    String privacy = sharedPref.getString("privacy", "false");
+
+    if(privacy.equals("true")){
+      super.initialize(cordova, webView);
+      Activity activity = this.cordova.getActivity();
+      activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+    }
   }
 }
